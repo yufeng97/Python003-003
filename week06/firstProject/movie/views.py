dad6968864119bff2c9680016aadb3f1d398fb6c
yufeng97 <47183963+yufeng97@users.noreply.github.com>
2020-11-04 19:53:33 +0800

    add search function

--- week06/firstProject/movie/views.py
+++ week06/firstProject/movie/views.py
@@ -4,5 +4,12 @@ from .models import Ratings
 
 
 def index(request):
-    ratings = Ratings.objects.all()
+    conditions = {'star__gt': 3}
+
+    search = request.POST.get('search', '')
+
+    if search:
+        conditions['comment__contains'] = search
+    ratings = Ratings.objects.filter(**conditions)
+    
     return render(request, 'index.html', locals())
