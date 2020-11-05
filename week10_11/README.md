7b91742ca9054829c9b075d693277f8e41b6317b
yufeng97 <47183963+yufeng97@users.noreply.github.com>
2020-11-05 19:44:06 +0800

    add README

---
+++ week10_11/README.md
@@ -0,0 +1,2 @@
+- 测试用户名：admin，密码：admin123
+- 测试用户名：jerry，密码：jerry001
--- week10_11/mysite/app/urls.py
+++ week10_11/mysite/app/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('index', views.index, name='index'),
     path('login', views.login, name='login'),
     path('logout', views.logout, name='logout'),
-    path('home', views.home),
+    path('home', views.home, name='home'),
 ]
